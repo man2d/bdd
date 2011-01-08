dc25a81d5c182ea67d4bfae939904636a93785be
Магистр <d2@MacBook-Pro-andrei-pryalkin.local>
2011-01-09 01:16:43 +0300

    Bare Rails 3.0.3 application with BDD environment

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'mysql2'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'email_spec'
+  gem 'timecop'
+  gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bdd::Application.config.secret_token = '4e4cc029d89e2b7a49b8cc342357a66be0adee33adb867c7df13a368602a2ef668b28280e4af254ad6293887d25e7e19917d0c6f5ec9d12c7662704da0453007'
